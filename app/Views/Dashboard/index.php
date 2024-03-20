8d6d6d4343711b07a5ad166ae8746bbbb3c66699
BritanyOlguin <121037879@upq.edu.mx>
2024-03-20 17:44:22 -0600

    Mejoras de vista dashboard
    Eliminar apartado Home y cambiarlo por servicios, para trabajar una sola vista

--- app/Views/Dashboard/index.php
+++ app/Views/Dashboard/index.php
@@ -1,8 +1,6 @@
 <?= $this->extend('layouts/dashboard-layout') ?>
 
 <?= $this->section('content') ?>
-    <h1>Dashboard</h1>
-    <p>Bienvenido a tu dashboard</p>
-
-<a href="<?= base_url('logout') ?>">Cerrar Sesión</a>
+    <h1>Servicios</h1>
+    <p>Bienvenido a tu sección servicios</p>
 <?= $this->endSection() ?>
--- app/Views/Layouts/dashboard-layout.php
+++ app/Views/Layouts/dashboard-layout.php
@@ -25,7 +25,7 @@
                     <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
                 </li>
                 <li class="nav-item d-none d-sm-inline-block">
-                    <a href="dashboard" class="nav-link">Home</a>
+                    <a href="<?= base_url('logout') ?>" class="nav-link">Cerrar Sesión</a>
                 </li>
             </ul>
 
@@ -93,23 +93,6 @@
 
         <!-- Content Wrapper. Contains page content -->
         <div class="content-wrapper">
-            <!-- Content Header (Page header) -->
-            <div class="content-header">
-                <div class="container-fluid">
-                    <div class="row mb-2">
-                        <div class="col-sm-6">
-                            <h1 class="m-0">Starter Page</h1>
-                        </div><!-- /.col -->
-                        <div class="col-sm-6">
-                            <ol class="breadcrumb float-sm-right">
-                                <li class="breadcrumb-item"><a href="<?= route_to('dashboard') ?>">Home</a></li>
-                            </ol>
-                        </div><!-- /.col -->
-                    </div><!-- /.row -->
-                </div><!-- /.container-fluid -->
-            </div>
-            <!-- /.content-header -->
-
             <!-- Main content -->
             <div class="content">
                 <div class="container-fluid">
